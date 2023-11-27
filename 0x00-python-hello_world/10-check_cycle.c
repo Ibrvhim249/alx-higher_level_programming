80dcf3d9589bda001fd7e7149015d140f917d00e
Ibrvhim249 <ibrahim.ehab01@gmail.com>
2023-11-27 14:30:17 -0800

    this is me

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,20 +1,31 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "list.h"
+#include "lists.h"
+
 /**
- * check_cycle - checks if link cyc
- * @list: pointer to list
- * Return: 1 if cyc , 0 else
+ * check_cycle - check for loop in LL
+ * @list: head of linked list
+ *
+ * Description - check for loops in LL
+ * Return: 1 if cycled, 0 if not
  */
+
 int check_cycle(listint_t *list)
 {
-	listint_t *slow = list, *fast = list;
-	while (fast && fast->next);
+	listint_t *slow, *fast;
+
+	if (!list)
 	{
-		slow = slow->next;
-		fast = fast=>next=>next;
+		return (0);
+	}
+	slow = list;
+	fast = list->next;
+	while (fast && slow && fast->next)
+	{
 		if (slow == fast)
+		{
 			return (1);
+		}
+		slow = slow->next;
+		fast = fast->next->next;
 	}
 	return (0);
 }
